748baf78095a6fb38ee66a168ba12a298629f19f
Deng Rodil <deng.rodil@gmail.com>
2025-08-15 17:29:17 +0800

    Imported proejct

---
+++ Assets/Sylpheed/GameFramework/ConfigRepository.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+using System.Linq;
+
+public class ConfigRepository : MonoBehaviour
+{
+    private static ConfigRepository instance;
+
+    public Object[] Configs;
+
+    private void Awake()
+    {
+        Assert.IsNull(instance, "There can only be one ConfigRepository");
+        instance = this;
+    }
+
+    public static T GetConfig<T>()
+        where T : Object
+    {
+        T settings = instance.Configs.Where(s => s is T).FirstOrDefault() as T;
+        Assert.IsNotNull(settings, typeof(T).ToString() + " does not exist");
+
+        return settings;
+    }
+}
---
+++ Assets/Sylpheed/GameFramework/PlayerState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Persistent data bound to a Player
+/// PlayerState does not get destroyed between scene loads
+/// </summary>
+public abstract class PlayerState : MonoBehaviour
+{
+    [Header("Serialization")]
+    public string SerializationKey;
+
+    // Should only be modified by the owning Player
+    public Player Owner { get; set; }
+
+    public T GetSiblingState<T>()
+        where T : PlayerState
+    {
+        return Owner.GetState<T>();
+    }
+}
